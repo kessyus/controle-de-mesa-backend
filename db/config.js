ef2f8ba12b88a77e2df3539636c4e2498606477e
Gabriela Da Silva <gabriela@Gabrielas-MacBook-Pro.local>
2021-04-22 11:34:07 -0300

    deploy heroku

--- db/config.js
+++ db/config.js
@@ -1,15 +1,23 @@
-const node_environment = process.env.NODE_ENV || 'development';
+// const node_environment = process.env.NODE_ENV || 'development';
 
-if (node_environment === 'development') {
-    require('dotenv').config();
-}
+// if (node_environment === 'development') {
+//     require('dotenv').config();
+// }
+
+// module.exports = {
+//     database: process.env.DB_NAME,
+//     username: process.env.DB_USERNAME,
+//     password: process.env.DB_PASSWORD,
+//     host: process.env.DB_HOST,
+//     port: process.env.DB_PORT,
+//     dialect: process.env.DB_DIALECT,
+//     logging: false
+// }
 
 module.exports = {
-    database: process.env.DB_NAME,
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: process.env.DB_DIALECT,
-    logging: false
-}
+    database: "heroku_2f279868511f576",
+    username: "ba932b6c79c2aa",
+    password: "b5c6cbf0",
+    host: "us-cdbr-east-03.cleardb.com",
+    dialect: "mysql",
+  };
