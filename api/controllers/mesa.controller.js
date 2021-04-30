72e79496a1e149325bf4d9f7db00ad823d92ca40
Kessyus Fófano <kessyus@gmail.com>
2021-04-30 12:48:41 -0300

    ordenando a lista de mesas

--- api/controllers/mesa.controller.js
+++ api/controllers/mesa.controller.js
@@ -1,5 +1,6 @@
 const { mesa, cardapios, mesaCardapio } = require('../models');
-const mesaService = require('../services/mesa.service')
+const mesaService = require('../services/mesa.service');
+const Sequelize = require('sequelize');
 
 const getAllMesas = async (req, res, next) => {
   const result = await mesa.findAll({
@@ -9,6 +10,7 @@ const getAllMesas = async (req, res, next) => {
         include: [{ model: cardapios }],
       },
     ],
+    order: Sequelize.literal(`mesa.numero`),
   });
 
   res.status(200).send(
@@ -27,137 +29,118 @@ const getAllMesas = async (req, res, next) => {
 };
 
 const getMesaById = async (req, res, next) => {
-
   try {
-  
     const result = await mesa.findOne({
       where: {
-        id: req.params.id
+        id: req.params.id,
       },
       include: {
         model: mesaCardapio,
         include: {
-          model: cardapios
-        }
-      }
+          model: cardapios,
+        },
+      },
     });
 
     res.status(200).send(result);
-   
   } catch (error) {
-    
     console.log(error);
-    res.status(500).send({ message: `Mesa ${req.params.id} não foi encontrada.` });
-
+    res
+      .status(500)
+      .send({ message: `Mesa ${req.params.id} não foi encontrada.` });
   }
-}
+};
 
 const postCadastroPedido = async (req, res, next) => {
-
   try {
-    
     // construir o model para incluir no cadastro
     const model = {
       status: true,
       id_cardapio: req.body.idcardapio,
       id_mesa: req.params.id,
-      dateTime: new Date().toISOString().slice(0, 19).replace('T', ' ')
-    }
+      dateTime: new Date().toISOString().slice(0, 19).replace('T', ' '),
+    };
 
     // grava no banco
     await mesaCardapio.create(model);
-    
+
     res.status(200).send({ message: 'Pedido gravado com sucesso.' });
-    
   } catch (error) {
-   
     console.log(error);
     res.status(500).send({ message: 'Erro interno do servidor.' });
-
   }
-}
+};
 
 const deletePedido = async (req, res, next) => {
-
   try {
-   
     await mesaCardapio.destroy({
       where: {
-        id: req.params.id
-      }
+        id: req.params.id,
+      },
     });
 
     res.status(200).send({ message: 'Pedido excluído com sucesso.' });
-
   } catch (error) {
-    
     console.log(error);
     res.status(500).send({ message: 'Erro interno do servidor.' });
   }
-}
+};
 
 //v2
 
 const criarMesa = async (req, res, next) => {
- 
   try {
     const { body } = req;
     const validarMesa = await mesaService.mesaExiste(body.numero);
-     
-    
+
     if (validarMesa) {
       return res.status(400).send({
-        mensagem: "Mesa já cadastrada"
+        mensagem: 'Mesa já cadastrada',
       });
     }
-    await mesaService.criarMesa(body)
-    
+    await mesaService.criarMesa(body);
+
     res.status(200).send({
-      mensagem: "mesa criada"
-    })
-    
+      mensagem: 'mesa criada',
+    });
   } catch (error) {
     res.status(500).send({
-      mensagem: 'error'
+      mensagem: 'error',
     });
-    }
-}
+  }
+};
 
 const alterarMesa = async (req, res, next) => {
   try {
     const { id } = req.params;
-    
+
     await mesaService.alterarMesa(id, req.body);
 
     return res.status(200).send({
-      mensagem: 'Mesa alterada.'
+      mensagem: 'Mesa alterada.',
     });
-
   } catch (error) {
     console.log(error);
     return req.status(500).send({
-      mensagem: 'Erro!!!'
-    })
-    
+      mensagem: 'Erro!!!',
+    });
   }
-}
+};
 
 const deletarMesa = async (req, res, next) => {
   try {
     const { params } = req;
-    await mesaService.deletarMesa(params.id)
-    
-    return res.status(200).send({
-      mensagem: 'mesa deletada'
-    })
-  } catch (error) {
-      return res.status(500).send({
-        mensagem: 'Internal server error!'
-      })
-  }
-  
+    await mesaService.deletarMesa(params.id);
 
-}
+    return res.status(200).send({
+      mensagem: 'mesa deletada',
+    });
+  } catch (error) {
+    return res.status(500).send({
+      mensagem: 'Internal server error!',
+    });
+  }
+};
 
 module.exports = {
   getAllMesas,
@@ -166,5 +149,5 @@ module.exports = {
   deletePedido,
   criarMesa,
   alterarMesa,
-  deletarMesa
-}
+  deletarMesa,
+};
